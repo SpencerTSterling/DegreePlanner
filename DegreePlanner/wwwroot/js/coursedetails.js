cb0a88c60dc915f3eb381f22c0f4334f75b99f71
Spencer Sterling <spencer.sterling2001@gmail.com>
2024-09-23 23:15:15 -0700

    Scroll bar on Courseitems in Course Detail view

---
+++ DegreePlanner/wwwroot/js/coursedetails.js
@@ -0,0 +1,21 @@
+﻿// Function to mark course item as complete
+function markAsComplete(itemId, checkbox) {
+    console.log('Checkbox clicked for item ID:', itemId, 'Completed:', checkbox.checked);
+    var isCompleted = checkbox.checked;
+
+    $.post('/Course/MarkCourseItemAsComplete', { id: itemId, isCompleted: isCompleted })
+        .done(function () {
+            if (isCompleted) {
+                $(checkbox).closest('.list-group-item').addClass('list-group-item-success');
+            } else {
+                $(checkbox).closest('.list-group-item').removeClass('list-group-item-success');
+            }
+        })
+        .fail(function () {
+            console.error('Failed to update course item.');
+        });
+}
+
+$(document).ready(function () {
+    // Additional logic for the Course Detail page can go here if needed.
+});
