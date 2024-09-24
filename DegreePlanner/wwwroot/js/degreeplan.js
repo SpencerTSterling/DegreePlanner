79692a281441fdad3005529f6703112b7356af9d
Spencer Sterling <spencer.sterling2001@gmail.com>
2024-09-24 00:49:15 -0700

    Clicking the edit/delete buttons on terms

--- DegreePlanner/wwwroot/js/degreeplan.js
+++ DegreePlanner/wwwroot/js/degreeplan.js
@@ -10,30 +10,27 @@
         });
 }
 $(document).ready(function () {
-    // Caret toggle logic
+    // Handle the click event for term-header to toggle collapse
     $('.term-header').click(function (e) {
-        e.preventDefault(); // Prevent default behavior
+        // Only toggle collapse if not clicking on Edit/Delete icons
+        var isIconClick = $(e.target).closest('.edit-term, .delete-term').length > 0;
 
-        var target = $(this).attr('href');  // Get the target collapse ID
-        $(target).collapse('toggle');  // Toggle collapse
+        if (!isIconClick) {
+            var target = $(this).attr('href');  // Get the collapse target from href attribute
+            $(target).collapse('toggle');  // Toggle collapse
 
-        // Toggle the caret icon
-        var $caret = $(this).find('.term-caret');
-        if ($caret.hasClass('bi-caret-down-fill')) {
-            $caret.removeClass('bi-caret-down-fill').addClass('bi-caret-up-fill');
-        } else {
-            $caret.removeClass('bi-caret-up-fill').addClass('bi-caret-down-fill');
+            // Toggle the caret icon
+            var $caret = $(this).find('.term-caret');
+            if ($caret.hasClass('bi-caret-down-fill')) {
+                $caret.removeClass('bi-caret-down-fill').addClass('bi-caret-up-fill');
+            } else {
+                $caret.removeClass('bi-caret-up-fill').addClass('bi-caret-down-fill');
+            }
         }
     });
 
-    // Expand and collapse all logic
-    $('#expandAll').click(function () {
-        $('.collapse').collapse('show'); // Show all collapsed sections
-        $('.term-caret').removeClass('bi-caret-down-fill').addClass('bi-caret-up-fill'); // Set all carets to up
-    });
-
-    $('#collapseAll').click(function () {
-        $('.collapse').collapse('hide'); // Hide all collapsed sections
-        $('.term-caret').removeClass('bi-caret-up-fill').addClass('bi-caret-down-fill'); // Set all carets to down
+    // Prevent collapse when clicking Edit/Delete icons
+    $('.edit-term, .delete-term').click(function (e) {
+        e.stopPropagation(); // Ensure the click event doesn't propagate to term-header
     });
 });
