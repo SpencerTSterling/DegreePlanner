a0b6129b51e527aa2d7c686426a417f87ef057e3
Spencer Sterling <spencer.sterling2001@gmail.com>
2024-11-02 00:53:15 -0700

    Reverted migrations back to intialcreate

--- DegreePlanner.DataAccess/Data/ApplicationDbContext.cs
+++ DegreePlanner.DataAccess/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace capstone.DegreePlanner.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
