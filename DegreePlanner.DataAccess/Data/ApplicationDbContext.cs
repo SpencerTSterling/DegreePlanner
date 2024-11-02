7c8ed1b970ec382fa81e4914493988192f802612
Spencer Sterling <spencer.sterling2001@gmail.com>
2024-11-02 00:27:36 -0700

    Change IdentityUser to User in ApplicationDbContext for custom user properties

--- DegreePlanner.DataAccess/Data/ApplicationDbContext.cs
+++ DegreePlanner.DataAccess/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace capstone.DegreePlanner.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
